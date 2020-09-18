84f249f3506d0c71d700b2b6b26b52605e9efe79
Alexander <webisforce@gmail.com>
2020-09-18 10:34:18 +0300

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,15 @@ export default (() => {
 			>
 				test netlify
 			</Text>
+			<Text
+				as="h2"
+				font="--headline1"
+				md-font="--headline2"
+				margin="20px 0 0 0"
+				color="#f4e7e7"
+			>
+				test netlify
+			</Text>
 			<Text as="p" font="--lead" margin="20px 0 0 0">
 				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
